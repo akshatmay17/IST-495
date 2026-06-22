e7843573e36816f89b0f4652a88b833be6a18b9e
akshatmay17 <akshat.may17@gmail.com>
2026-06-22 01:28:15 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="manifest" href="/manifest.json"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300..700;1,9..144,400..600&family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
       </head>
       <body style={{margin:0,padding:0,WebkitFontSmoothing:"antialiased"}}>
         {children}
